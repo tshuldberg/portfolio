841cd9a931f137f6f3ed3809ee0c3a3f989b359e
Trey Shuldberg <trey@Treys-MBP.attlocal.net>
2020-05-08 16:05:05 -0700

    changes

---
+++ scripts/modal.js
@@ -0,0 +1,136 @@
+$(document).ready(function () {
+  // MODAL
+  var modalText = {
+    travel: {
+      title: "Where Ya Goin",
+      tag: "TRAVEL BLOG.",
+      detail:
+        "Travel blog to create posts about the places this world has to offer and to share those experiences with the world.",
+      link: "https://github.com/tshuldberg/Travel",
+    },
+    drinking: {
+      title: "Coastr",
+      tag: "ONLINE DRINK ORDERING.",
+      detail:
+        "An ordering service for high influx bars and clubs to allow customers to order from the dance floor.",
+      link: "https://coastr-app.herokuapp.com/",
+    },
+    entertainment: {
+      title: "Get It Ticket",
+      tag: "TICKET REDISTRIBUTION.",
+      detail:
+        "Get It Ticket is an online source for event and concert ticket purchasing that will prevent sclaping and allow for easy resale of tickets in a pinch.",
+      link: "https://get-it-ticket.herokuapp.com/",
+    },
+    battleship: {
+      title: "Browser Battleship",
+      tag: "BATTLESHIP GAME",
+      detail: "An in browser BattleShip game with smart CPU guessing.",
+      link: "https://tshuldberg.github.io/Battleship-JS/",
+    },
+  };
+
+  $("#gallery .button").on("click", function () {
+    fillModal(this.id);
+    $(".modal-wrap").addClass("visible");
+  });
+
+  $(".close").on("click", function () {
+    $(".modal-wrap, #modal .button").removeClass("visible");
+  });
+
+  $(".mask").on("click", function () {
+    $(".modal-wrap, #modal .button").removeClass("visible");
+  });
+
+  var carousel = $("#carousel"),
+    slideWidth = 700,
+    threshold = slideWidth / 3,
+    dragStart,
+    dragEnd;
+
+  setDimensions();
+
+  $("#next").click(function () {
+    shiftSlide(-1);
+  });
+  $("#prev").click(function () {
+    shiftSlide(1);
+  });
+
+  carousel.on("mousedown", function () {
+    if (carousel.hasClass("transition")) return;
+    dragStart = event.pageX;
+    $(this).on("mousemove", function () {
+      dragEnd = event.pageX;
+      $(this).css("transform", "translateX(" + dragPos() + "px)");
+    });
+    $(document).on("mouseup", function () {
+      if (dragPos() > threshold) {
+        return shiftSlide(1);
+      }
+      if (dragPos() < -threshold) {
+        return shiftSlide(-1);
+      }
+      shiftSlide(0);
+    });
+  });
+
+  function setDimensions() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      slideWidth = $(window).innerWidth();
+    }
+    $(".carousel-wrap, .slide").css("width", slideWidth);
+    $(".modal").css("max-width", slideWidth);
+    $("#carousel").css("left", slideWidth * -1);
+  }
+
+  function dragPos() {
+    return dragEnd - dragStart;
+  }
+
+  function shiftSlide(direction) {
+    if (carousel.hasClass("transition")) return;
+    dragEnd = dragStart;
+    $(document).off("mouseup");
+    carousel
+      .off("mousemove")
+      .addClass("transition")
+      .css("transform", "translateX(" + direction * slideWidth + "px)");
+    setTimeout(function () {
+      if (direction === 1) {
+        $(".slide:first").before($(".slide:last"));
+      } else if (direction === -1) {
+        $(".slide:last").after($(".slide:first"));
+      }
+      carousel.removeClass("transition");
+      carousel.css("transform", "translateX(0px)");
+    }, 700);
+  }
+
+  function fillModal(id) {
+    $("#modal .title").text(modalText[id].title);
+    $("#modal .detail").text(modalText[id].detail);
+    $("#modal .tag").text(modalText[id].tag);
+    if (modalText[id].link)
+      $("#modal .button")
+        .addClass("visible")
+        .parent()
+        .attr("href", modalText[id].link);
+
+    $.each($("#modal li"), function (index, value) {
+      $(this).text(modalText[id].bullets[index]);
+    });
+    $.each($("#modal .slide"), function (index, value) {
+      $(this).css({
+        background:
+          "url('img/slides/" + id + "-" + index + ".jpg') center center/cover",
+        backgroundSize: "cover",
+      });
+    });
+  }
+});
